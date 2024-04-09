b352a3ff601b48f05dc3d8c6c738e150ac3a812e
Jonathan Zhou <34146693+zhou059@users.noreply.github.com>
2024-04-09 09:10:15 -0700

    Update orgInit.sh

--- orgInit.sh
+++ orgInit.sh
@@ -11,7 +11,7 @@ sfdx shane:user:password:set -p salesforce1 -g User -l User
 # Sharda data kit S3 = 04tHn000001eb4J
 sfdx force:package:install -p 04tHn000001eb4J -w 10
 
-sfdx force:source:push
+# sfdx force:source:push
 # sf automig dump -d data -o Account,Contact,Opportunity,Lead,Task,Campaign,Case,AccountContactRelation,CampaignMember,Branch__c
 sf automig load -d data
 sfdx force:org:open
